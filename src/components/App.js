542967c9c253d5ca54b2dee4b2a2b98caaaf705e
dorim1209 <dorim011@naver.com>
2019-11-18 11:42:35 +0900

    회원가입 만듦

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Login from './Login';
-import Nav from './Nav';
-import Header from './Header';
-import Home from './Home';
-import Footer from './Footer';
-import Post from './Post';
+import Login from "./Login";
+import Nav from "./Nav";
+import Header from "./Header";
+import Home from "./Home";
+import Footer from "./Footer";
+import Post from "./Post";
+import Join from "./Join";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/post/:id" component={Post} />
+      <Route path="/join" component={Join} />
       <Footer />
     </BrowserRouter>
   );
